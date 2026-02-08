f9bcadaf4952a377b1a8f2f99245f5d3cc48ff1f
BRY4N18 <blombeidae@uteq.edu.ec>
2026-02-08 13:22:17 -0500

    jaja

---
+++ src/main/java/com/GrupoA/IntegracionJSF/repository/CargoRepository.java
@@ -0,0 +1,9 @@
+package com.GrupoA.IntegracionJSF.repository;
+
+import com.GrupoA.IntegracionJSF.model.Cargo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CargoRepository extends JpaRepository<Cargo, Integer> {
+}
