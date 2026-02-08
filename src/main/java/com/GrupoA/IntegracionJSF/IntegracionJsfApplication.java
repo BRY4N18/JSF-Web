d37e570ee489f30a8f8b651705cfa4e9707226b4
BRY4N18 <blombeidae@uteq.edu.ec>
2026-02-08 12:12:38 -0500

    Primer subida

---
+++ src/main/java/com/GrupoA/IntegracionJSF/IntegracionJsfApplication.java
@@ -0,0 +1,13 @@
+package com.GrupoA.IntegracionJSF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracionJsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracionJsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GrupoA/IntegracionJSF/IntegracionJsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GrupoA.IntegracionJSF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracionJsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
